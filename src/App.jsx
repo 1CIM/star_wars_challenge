4a03372eeae2d96d4372b27eaf5ae2a82a22cc87
kim_haaga <kim.haaga@live.se>
2021-04-13 18:50:06 +0200

    First commit base setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 id="header">Star Wars UwU</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
